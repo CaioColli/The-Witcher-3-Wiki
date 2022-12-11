be0bf6e9cb7dfa0ef447bd91b2cc7910e69a8181
Caio Colli <110783734+CaioColli@users.noreply.github.com>
2022-12-10 21:26:24 -0300

    Atualização 10/12/22
    Implementei uma nova página HTML, Css e novos códigos JavaScrpt com função de mostrar e esconder texto

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,50 @@
-/* Set the width of the side navigation to 250px */
+
+// Menu Lateral Mobile
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+// Menu Lateral Mobile
+
+// Mostrar mais
+
+function leiaMais() {
+  var pontos=document.getElementById("pontos");
+  var maisTexto=document.getElementById("mais");
+  var lerMais=document.getElementById("lerMais");
+
+  if(pontos.style.display === "none"){
+    pontos.style.display="inline";
+    maisTexto.style.display="none";
+    lerMais.innerHTML="Ler Mais";
+  }else{
+    pontos.style.display="none";
+    maisTexto.style.display="inline";
+    lerMais.innerHTML="Ler Menos";
+  }
+}
+
+function MaisEnredo() {
+  var varPontos=document.getElementById("pontosEnredo");
+  var varMaisEnredo=document.getElementById("maisEnredo");
+  var btnEnredo=document.getElementById("btnEnredo");
+
+  if(varPontos.style.display === "none"){
+    varPontos.style.display="inline";
+    varMaisEnredo.style.display="none";
+    btnEnredo.innerHTML="Ler Mais";
+  }else{
+    varPontos.style.display="none";
+    varMaisEnredo.style.display="inline";
+    btnEnredo.innerHTML="Ler Menos";
+  }
+}
+
+
+
+// Mostrar mais
